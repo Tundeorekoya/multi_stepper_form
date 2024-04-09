7e5926eaddf028b0608800f42868ba48837ee158
tundeorekoya <tundeorekoya1998@gmail.com>
2024-04-09 10:29:30 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css'
+import { Account, Details, Final } from './components'
+import { useState } from 'react'
+import Stepper from './components/Stepper'
+import StepperControl from './components/StepperControl'
+
+function App() {
+  const [currentStep, setCurrentStep] = useState(1)
+  const steps = [
+    "Account Information",
+    "Personal Details",
+    "Complete",
+  ]
+  const displayStep = (step) => {
+    switch (step) {
+      case 1:
+        return <Account />
+      case 2:
+        return <Details />
+      case 3:
+        return <Final />
+    }
+  }
+  const handleClick = (direction) => {
+    let newSteps = currentStep
+    direction === "Next" ? newSteps++ : newSteps--
+
+    // check if the steps are within bounds
+
+    newSteps > 0 && newSteps <= steps.length && setCurrentStep(newSteps)
+
+  }
+  return (
+    <>
+      <div className='md:w-1/2 mx-auto shadow-xl rounded-2xl mt-8 pb-2 p-4 bg-white'>
+        <div className='container vertical justify-center'>
+          <Stepper steps={steps} currentStep={currentStep} />
+          <StepperControl handleClick={handleClick}
+            steps={steps}
+            currentStep={currentStep} />
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Account from "./Account"
+import Details from "./Details";
+import Final from "./Final";
+import Payment from "./Payment";
+
+export {Account,Details,Final,Payment}
