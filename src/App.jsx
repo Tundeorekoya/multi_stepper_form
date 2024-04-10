471d4cb0f1d7cd55cc99db764946b9ded102ec8a
tundeorekoya <tundeorekoya1998@gmail.com>
2024-04-10 02:11:51 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Account, Details, Final } from './components'
 import { useState } from 'react'
 import Stepper from './components/Stepper'
 import StepperControl from './components/StepperControl'
+import { StepperContext } from './components/context/stepperContext'
 
 function App() {
   const [currentStep, setCurrentStep] = useState(1)
+  const [userInfo, setUserInfo] = useState("")
+  const [finalInfo, setFinalInfo] = useState([])
   const steps = [
     "Account Information",
     "Personal Details",
@@ -35,10 +38,15 @@ function App() {
       <div className='md:w-1/2 mx-auto shadow-xl rounded-2xl mt-8 pb-2 p-4 bg-white'>
         <div className='container vertical justify-center'>
           <Stepper steps={steps} currentStep={currentStep} />
-          <StepperControl handleClick={handleClick}
-            steps={steps}
-            currentStep={currentStep} />
+          <div className='py-20 px-10 items-center'>
+            <StepperContext.Provider value={{ userInfo, setUserInfo, finalInfo, setFinalInfo }}>
+              {displayStep(currentStep)}
+            </StepperContext.Provider>
+          </div>
+          
+          <StepperControl handleClick={handleClick} steps={steps} currentStep={currentStep} />
         </div>
+
       </div>
 
     </>
--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import { StepperContext } from "./context/stepperContext"
+import { useContext } from "react"
 const Account = () => {
+  const { userInfo, setUserInfo } = useContext(StepperContext)
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUserInfo({ ...userInfo, [name]: value })
+  }
   return (
-    <div>Account</div>
+    <div className="flex flex-col">
+      <div className="w-full flex-1 mx-2">
+        <div className="font-bold h-6 my-2 uppercase text-grey-500 text-xs leading-8">UserName</div>
+        <input type="text" onChange={handleChange} value={userInfo["username"] || "" } name="username" placeholder="Username" className="p-1 px-2 border rounded-md w-full  outline-none text-gray-800"/>
+      </div>
+      <div className="w-full flex-1 mx-2">
+        <div className="font-bold h-6 my-2 uppercase text-grey-500 text-xs leading-8">Password</div>
+        <input type="Password" onChange={handleChange} value={userInfo["Password"] || ""} name="Password" placeholder="Password" className="p-1 px-2 border rounded-md w-full  outline-none text-gray-800" />
+      </div>
+
+
+    </div>
   )
 }
 
