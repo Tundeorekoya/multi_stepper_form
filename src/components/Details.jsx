ee396fc6b795c5b925ed631874048bcde042f7bd
tundeorekoya <tundeorekoya1998@gmail.com>
2024-04-10 03:41:13 -0700

    update

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { StepperContext } from "./context/stepperContext"
+import { useContext } from "react"
 
 const Details = () => {
+  const { userInfo, setUserInfo } = useContext(StepperContext)
+  const handleChange = (e) => {
+    const { name, value } = e.target
+      setUserInfo({ ...userInfo, [name]: value })
+
+  }
   return (
-    <div>Details</div>
+   <>
+      <div className="grid grid-cols-2 gap-2">
+        <div className="w-full  flex-1 mx-2  flex-nowrap">
+          <div className="font-bold h-6 my-2 uppercase text-grey-500 text-xs leading-8 ">First Name</div>
+          <input type="text" onChange={handleChange} value={userInfo["Name"] || ""} name="Name" placeholder="Name" className="p-1 px-2 border rounded-md w-full  outline-none text-gray-800" />
+        </div>
+        <div className="w-full flex-1 mx-2 ">
+          <div className="font-bold h-6 my-2 uppercase text-grey-500 text-xs leading-8">Password</div>
+          <input type="Password" onChange={handleChange} value={userInfo["Password"] || ""} name="Password" placeholder="Password" className="p-1 px-2 border rounded-md w-full  outline-none text-gray-800" />
+        </div>
+        <div className="w-full flex-1 mx-2 col-span-2 ">
+          <div className="font-bold h-6 my-2 uppercase text-grey-500 text-xs leading-8 ">Password</div>
+          <input type="Password" onChange={handleChange} value={userInfo["Password"] || ""} name="Password" placeholder="Password" className="p-1 px-2 border rounded-md w-full  outline-none text-gray-800" />
+        </div>
+        <div className="w-full flex-1 mx-2 col-span-2">
+          <div className="font-bold h-6 my-2 uppercase text-grey-500 text-xs leading-8">Password</div>
+          <input type="Password" onChange={handleChange} value={userInfo["Password"] || ""} name="Password" placeholder="Password" className="p-1 px-2 border rounded-md w-full  outline-none text-gray-800" />
+        </div>
+        <div className="w-full flex-1 mx-2 col-span-2">
+          <div className="font-bold h-6 my-2 uppercase text-grey-500 text-xs leading-8">Password</div>
+          <input type="Password" onChange={handleChange} value={userInfo["Password"] || ""} name="Password" placeholder="Password" className="p-1 px-2 border rounded-md w-full  outline-none text-gray-800" />
+        </div>
+
+
+      </div>
+   </>
   )
 }
 
--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -54,7 +54,7 @@ const Stepper = ({ steps, currentStep }) => {
       <div key={index} className={index !== newStep.length - 1 ? "w-full" : ""}>
         <div className="relative flex flex-row items-center text-teal-600" >
           <div className={`rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3 ${step.selected ? "bg-green-600 text-white font-bold border border-green-600 " :""}` }>{step.completed  ? (<span className='text-white font-bold text-xl'>&#10003;</span>):(index + 1)}</div>
-          <div className={`absolute top-0 mt-16 w-28 items-center text-xs font-medium uppercase ${step.highlighted ? "text-gray-900":"text-gray-400"}`} >{step.description}</div>
+          <div className={`absolute top-0 mt-16 w-16 items-center text-xs font-medium uppercase ${step.highlighted ? "text-gray-900":"text-gray-400"}`} >{step.description}</div>
           <div className={`flex-auto flex-col border-t-2 transition duration-500 ease-in-out ${step.completed ? "border-green-600":"text-gray-400" }`}></div>
         </div >
       </div>
